625d07bd817e5862eb3efb24a0ea5531975c54ee
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2022-12-08 15:45:23 +0100

    Day 7 part 1 finally works after a rewrite

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,52 +1,64 @@
-use std::{collections::HashSet, fs, thread::current};
+use std::{
+    cell::{Ref, RefCell},
+    collections::HashSet,
+    fs,
+    rc::Rc,
+    thread::current,
+};
 
 use regex::Regex;
 
-#[derive(Debug)]
-struct Dir {
-    abs_path: String,
-    name: String,
-    file_sizes: usize,
-    parent_dir: String,
+#[derive(PartialEq, Debug)]
+struct TreeNode {
+    pub name: String,
+    pub size: usize,
+    pub children: Vec<Rc<RefCell<TreeNode>>>,
+    pub parent: Option<Rc<RefCell<TreeNode>>>,
+    pub is_dir: bool,
+}
+
+impl TreeNode {
+    pub fn new(name: &str) -> TreeNode {
+        TreeNode {
+            name: name.to_string(),
+            size: 0,
+            children: vec![],
+            parent: None,
+            is_dir: false,
+        }
+    }
+
+    pub fn add_child(&mut self, new_node: Rc<RefCell<TreeNode>>) {
+        self.children.push(new_node);
+    }
+
+    pub fn get_size(&self) -> usize {
+        let mut size = 0;
+        size += self.size;
+
+        for child in self.children.iter() {
+            let child_clone = child.borrow();
+            size += child_clone.get_size();
+        }
+        size
+    }
 }
 
 fn main() {
-    let input = read_input("./src/jona.test");
-    // println!("{:?}", input);
-    let mut parent_dirs: HashSet<String> = HashSet::new();
-    input.iter().for_each(|dir| {
-        parent_dirs.insert(String::from(&dir.abs_path));
-    });
-    println!("{} {}", parent_dirs.len(), input.len());
-    part1(&input);
-}
-fn part1(input: &Vec<Dir>) {
-    // println!("{}", input.len());
-    let input_clone = input.clone();
-    let mut sizes: Vec<(&String, usize)> = input_clone
-        .iter()
-        .map(|dir| {
-            let size = get_dir_size(&input_clone, &dir.abs_path);
-            (&dir.name, size)
-        })
-        .filter(|dir_size| dir_size.1 <= 100000)
-        .collect();
+    // let input = read_input("./src/jona.test");
+    let root_node = read_input("./src/input.prod");
 
-    let sum: usize = sizes.iter().map(|dir_size| dir_size.1).sum();
-    // println!("{:?}", sizes.len());
-
-    println!("{:?}", sizes);
-    println!("{:?}", sum);
+    let output_part1 = part1(root_node);
+    println!("Part 1: {}", output_part1);
+    // println!("Total size: {}", total);
 }
 
-fn read_input(filename: &str) -> Vec<Dir> {
-    let mut current_dir = String::from("");
-    let mut current_size = 0;
+fn read_input(filename: &str) -> Rc<RefCell<TreeNode>> {
     let cd_regex = Regex::new(r"cd (.*)").unwrap();
-    let size_regex = Regex::new(r"(\d+).*").unwrap();
-    let mut dirs: Vec<Dir> = Vec::new();
+    let size_regex = Regex::new(r"(\d+) (.*)").unwrap();
 
-    let all_dirs_regex = Regex::new(r"([a-z]+/)").unwrap();
+    let root = Rc::new(RefCell::new(TreeNode::new("root")));
+    let mut current = Rc::clone(&root);
 
     fs::read_to_string(filename)
         .expect("Error reading file")
@@ -55,99 +67,61 @@ fn read_input(filename: &str) -> Vec<Dir> {
             if line.contains("$") {
                 if cd_regex.is_match(line) {
                     let cap = cd_regex.captures(line).unwrap();
-                    push_current_dir_size(&current_dir, &mut current_size, &mut dirs);
                     if !(&cap[1] == ".." || &cap[1] == "/") {
-                        current_dir.push_str(&cap[1]);
-                        current_dir.push_str("/");
-                    } else if &cap[1] == ".." {
-                        if all_dirs_regex.is_match(&current_dir) {
-                            let child_cap = all_dirs_regex.find_iter(&current_dir).last().unwrap();
-                            let own_len = current_dir.len();
-                            let child_len = child_cap.as_str().len();
-                            current_dir.truncate(child_len - 1);
-                            // current_dir = current_dir.replace(child_cap.as_str(), "");
+                        let child = Rc::new(RefCell::new(TreeNode::new(&cap[1])));
+                        println!("Creating new child: {}", &cap[1]);
+                        current.borrow_mut().add_child(Rc::clone(&child));
+                        {
+                            let mut mut_child = child.borrow_mut();
+                            mut_child.is_dir = true;
+                            mut_child.parent = Some(Rc::clone(&current));
                         }
+                        current = child;
+                    } else if &cap[1] == ".." {
+                        let current_clone = Rc::clone(&current);
+                        current = Rc::clone(current_clone.borrow().parent.as_ref().unwrap());
                     } else {
                         return;
                     }
-                    // println!("{} Current dir: {}", line, current_dir);
                 }
             } else {
-                if !(current_dir == "") {
-                    if size_regex.is_match(line) {
-                        let size_cap = size_regex.captures(line).unwrap();
-                        let size = &size_cap[1].parse::<usize>().unwrap();
-                        current_size += size;
-                        // println!("{:?} {:?}", current_size, size);
-                    }
+                if size_regex.is_match(line) {
+                    let size_cap = size_regex.captures(line).unwrap();
+                    let size = &size_cap[1].parse::<usize>().unwrap();
+                    current.borrow_mut().size += size;
+                    // current_size += size;
                 }
             }
         });
-    push_current_dir_size(&current_dir, &mut current_size, &mut dirs);
-    dirs
+    root
 }
 
-fn push_current_dir_size(current_dir: &str, current_size: &mut usize, dirs: &mut Vec<Dir>) {
-    let all_dirs_regex = Regex::new(r"([a-z]+/)").unwrap();
-    if all_dirs_regex.is_match(&current_dir) {
-        // let parent_cap = all_dirs_regex.find_iter(&current_dir).nth(0).unwrap();
-        let parent_cap = all_dirs_regex
-            .find_iter(&current_dir)
-            .fold("".to_owned(), |parent_string, curr| {
-                parent_string + curr.as_str()
-            });
-        let cap_clone = parent_cap.clone();
-        let child_cap = all_dirs_regex.find_iter(&current_dir).last().unwrap();
-        let cap_count = all_dirs_regex.find_iter(&current_dir).count();
-        if cap_count > 1 {
-            if !dirs.iter().any(|dir| dir.abs_path == current_dir) {
-                dirs.push(Dir {
-                    abs_path: cap_clone,
-                    name: child_cap.as_str().replace("/", ""),
-                    file_sizes: *current_size,
-                    parent_dir: parent_cap.as_str().replace(child_cap.as_str(), ""),
-                });
-            }
+fn part1(root_node: Rc<RefCell<TreeNode>>) -> usize {
+    let mut size = 0;
 
-            *current_size = 0;
-        }
-        if cap_count == 1 {
-            if !(*current_size == 0) {
-                let name = parent_cap.as_str().replace("/", "");
-                if !dirs.iter().any(|dir| dir.abs_path == current_dir) {
-                    dirs.push(Dir {
-                        abs_path: parent_cap,
-                        name,
-                        file_sizes: *current_size,
-                        parent_dir: "".to_owned(),
-                    });
-                }
-                *current_size = 0;
-            }
-        }
+    let root = root_node.borrow();
+
+    if root.get_size() <= 100000 {
+        size += root.get_size();
     }
+    for child in root.children.iter() {
+        println!(
+            "Found child: {} with size: {}",
+            child.borrow().name,
+            child.borrow().get_size()
+        );
+        size += part1(Rc::clone(&child));
+    }
+    size
 }
 
-fn get_dir_size(input: &[Dir], path: &str) -> usize {
-    // let mut size = 0;
-    // if size == 0 {
-    //     println!("initial call");
-    // }
-    if path == "/" {
-        println!("Root dir");
+fn total_size(root_node: Rc<RefCell<TreeNode>>) -> usize {
+    let mut size = 0;
+    let root = root_node.borrow();
+
+    size += root.size;
+    for child in root.children.iter() {
+        size += total_size(Rc::clone(&child));
     }
-    for (index, possible_child) in input.iter().enumerate() {
-        if possible_child.abs_path == path {
-            // size += possible_child.file_sizes;
-            return possible_child.file_sizes
-                + get_dir_size(&input[index + 1..], &possible_child.abs_path);
-        }
-        if possible_child.parent_dir == path {
-            // size += get_dir_size(&input[index..], &possible_child.name);
-            return possible_child.file_sizes
-                + get_dir_size(&input[index + 1..], &possible_child.abs_path);
-        }
-    }
-    0
-    // return size;
+    size
 }
